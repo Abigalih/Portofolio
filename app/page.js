24d25c5428c07e5d6d71498814c290ca58ccd851
Abiyyu Galih Ghifari <92355182+Abigalih@users.noreply.github.com>
2023-11-20 00:33:37 +0700

    color revision

--- app/page.js
+++ app/page.js
@@ -3,7 +3,7 @@ import HeroSection from './component/HeroSection'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col bg-[#121212]">
+    <main className="flex min-h-screen flex-col bg-[#191919]">
       <div class="container mx-auto py-4 px-12">
         <HeroSection />
       </div>
